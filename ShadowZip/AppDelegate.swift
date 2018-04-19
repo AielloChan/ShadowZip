f9c58c6fab1deb09a817cf61480b6465211b4950
Aiello Chan <Aiello.Chan@Gmail.com>
2018-04-19 18:19:30 +0800

    Initial Commit

---
+++ ShadowZip/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ShadowZip
+//
+//  Created by 陈晓龙 on 19/04/2018.
+//  Copyright © 2018 陈晓龙. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ShadowZip/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ShadowZip
+//
+//  Created by 陈晓龙 on 19/04/2018.
+//  Copyright © 2018 陈晓龙. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
